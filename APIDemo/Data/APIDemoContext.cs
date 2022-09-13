fbfe9c04100497ba736867b16ad5cceb36755c23
shilpa <shilpasekhar20202@gmail.com>
2022-09-13 19:20:32 +0530

    13/9/22

---
+++ APIDemo/Data/APIDemoContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using APIDemo.Controllers.Models;
+
+namespace APIDemo.Data
+{
+    public class APIDemoContext : DbContext
+    {
+        public APIDemoContext (DbContextOptions<APIDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<APIDemo.Controllers.Models.User> User { get; set; } = default!;
+
+        public DbSet<APIDemo.Controllers.Models.ToDoItem>? ToDoItem { get; set; }
+    }
+}
---
+++ APIDemo/Services/TicketService.cs
@@ -0,0 +1,53 @@
+﻿using APIDemo.Controllers.Models;
+using System.Xml.Linq;
+
+namespace APIDemo.Services
+{
+    public class TicketService
+    {
+        static List<Ticket> Tickets { get; }
+        static int nextId = 3;
+        private static Ticket ticket;
+
+        static TicketService()
+        {
+            Tickets = new List<Ticket>
+                {
+                   new Ticket{ Id= 1, BookedFor = "Movie1",Price=5000},
+                   new Ticket{ Id= 2, BookedFor = "Movie2",Price=4000},
+                };
+        }
+
+        public static List<Ticket> GetAll() => Tickets;
+
+        public static Ticket? Get(int id) => Tickets.FirstOrDefault(p => p.Id == id);
+
+        public static void Add(Ticket Tickets)
+        {
+            Tickets.Id = nextId++;
+            TicketService.Tickets.Add(Tickets);
+        }
+
+        public static void Delete(int id)
+        {
+            var Ticket = Get(id);
+            if (Ticket is null)
+                return;
+
+            Tickets.Remove(Ticket);
+        }
+
+        public static void Update(Ticket Ticket)
+        {
+            var index = Tickets.FindIndex(t => t.Id == Ticket.Id);
+            if (index == -1)
+                return;
+
+            Tickets[index] = ticket;
+        }
+    }
+}
+
+
+    
+
