323c09b91d12f2b47b6aa60b66960daddd7e57c6
shilpa <shilpasekhar20202@gmail.com>
2022-09-15 18:00:33 +0530

    15/9/22

---
+++ TDDemo/Rooter.cs
@@ -0,0 +1,15 @@
+﻿namespace TDDemo
+{
+    internal class Rooter
+    {
+        internal double SquareRoot(double input)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal double SqurareRoot(double input)
+        {
+            return Math.Sqrt(input);
+        }
+    }
+}
